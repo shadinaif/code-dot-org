f7cb85152a8fb8fca2621458b5ad17aaba80b711
Dani McAvoy <danimcavoy@gmail.com>
2020-04-30 20:35:32 -0400

    Update stage to lesson and flex category to lesson group in editorReduxTest

--- apps/test/unit/code-studio/editorReduxTest.js
+++ apps/test/unit/code-studio/editorReduxTest.js
@@ -13,7 +13,7 @@ import reducers, {
 
 const getInitialState = () => ({
   levelKeyList: {},
-  stages: [
+  lessons: [
     {
       id: 100,
       name: 'A',
@@ -56,11 +56,11 @@ describe('editorRedux reducer tests', () => {
   beforeEach(() => (initialState = getInitialState()));
 
   it('reorder levels', () => {
-    const nextState = reducer(initialState, reorderLevel(1, 3, 1)).stages;
+    const nextState = reducer(initialState, reorderLevel(1, 3, 1)).lessons;
     assert.deepEqual(nextState[0].levels.map(l => l.activeId), [5, 1, 4, 6]);
   });
   it('moves level to stage', () => {
-    const nextState = reducer(initialState, moveLevelToLesson(1, 3, 2)).stages;
+    const nextState = reducer(initialState, moveLevelToLesson(1, 3, 2)).lessons;
     assert.deepEqual(nextState[0].levels.map(l => l.activeId), [1, 4, 6]);
     assert.deepEqual(nextState[1].levels.map(l => l.activeId), [3, 5]);
   });
@@ -68,26 +68,27 @@ describe('editorRedux reducer tests', () => {
     const nextState = reducer(
       initialState,
       addGroup('New Stage 1', 'New Group')
-    ).stages;
+    ).lessons;
     assert.equal(nextState[nextState.length - 1].name, 'New Stage 1');
   });
   it('add stage', () => {
-    const nextState = reducer(initialState, addLesson(1, 'New Stage 2')).stages;
+    const nextState = reducer(initialState, addLesson(1, 'New Stage 2'))
+      .lessons;
     assert.deepEqual(nextState.map(s => s.name), ['A', 'New Stage 2', 'B']);
   });
   it('set active variant', () => {
-    const nextState = reducer(initialState, setActiveVariant(2, 1, 2)).stages;
+    const nextState = reducer(initialState, setActiveVariant(2, 1, 2)).lessons;
     assert.equal(nextState[1].levels[0].activeId, 2);
   });
   it('set level field', () => {
     let nextState = reducer(initialState, setField(1, 1, {videoKey: '_a_'}));
-    assert.equal(nextState.stages[0].levels[0].videoKey, '_a_');
+    assert.equal(nextState.lessons[0].levels[0].videoKey, '_a_');
     nextState = reducer(nextState, setField(1, 1, {skin: '_b_'}));
-    assert.equal(nextState.stages[0].levels[0].skin, '_b_');
+    assert.equal(nextState.lessons[0].levels[0].skin, '_b_');
     nextState = reducer(nextState, setField(1, 1, {conceptDifficulty: '_c_'}));
-    assert.equal(nextState.stages[0].levels[0].conceptDifficulty, '_c_');
+    assert.equal(nextState.lessons[0].levels[0].conceptDifficulty, '_c_');
     nextState = reducer(nextState, setField(1, 1, {concepts: '_d_'}));
-    assert.equal(nextState.stages[0].levels[0].concepts, '_d_');
+    assert.equal(nextState.lessons[0].levels[0].concepts, '_d_');
   });
 
   describe('lesson groups', () => {
@@ -95,52 +96,52 @@ describe('editorRedux reducer tests', () => {
 
     beforeEach(() => {
       initialStages = [
-        {flex_category: 'X', id: 101, position: 1, relativePosition: 1},
-        {flex_category: 'X', id: 102, position: 2, relativePosition: 2},
-        {flex_category: 'Y', id: 103, position: 3, relativePosition: 3},
-        {flex_category: 'Y', id: 104, position: 4, relativePosition: 4}
+        {lesson_group: 'X', id: 101, position: 1, relativePosition: 1},
+        {lesson_group: 'X', id: 102, position: 2, relativePosition: 2},
+        {lesson_group: 'Y', id: 103, position: 3, relativePosition: 3},
+        {lesson_group: 'Y', id: 104, position: 4, relativePosition: 4}
       ];
-      initialState.stages = initialStages;
+      initialState.lessons = initialStages;
     });
 
     it('moves a stage up three times', () => {
       const id = 104;
-      let position = initialState.stages.find(s => s.id === id).position;
+      let position = initialState.lessons.find(s => s.id === id).position;
       let state = reducer(initialState, moveLesson(position, 'up'));
       assert.deepEqual(
         [
-          {flex_category: 'X', id: 101, position: 1, relativePosition: 1},
-          {flex_category: 'X', id: 102, position: 2, relativePosition: 2},
-          {flex_category: 'Y', id: 104, position: 3, relativePosition: 3},
-          {flex_category: 'Y', id: 103, position: 4, relativePosition: 4}
+          {lesson_group: 'X', id: 101, position: 1, relativePosition: 1},
+          {lesson_group: 'X', id: 102, position: 2, relativePosition: 2},
+          {lesson_group: 'Y', id: 104, position: 3, relativePosition: 3},
+          {lesson_group: 'Y', id: 103, position: 4, relativePosition: 4}
         ],
-        state.stages,
+        state.lessons,
         'first move changes position but not group'
       );
 
-      position = state.stages.find(s => s.id === id).position;
+      position = state.lessons.find(s => s.id === id).position;
       state = reducer(state, moveLesson(position, 'up'));
       assert.deepEqual(
         [
-          {flex_category: 'X', id: 101, position: 1, relativePosition: 1},
-          {flex_category: 'X', id: 102, position: 2, relativePosition: 2},
-          {flex_category: 'X', id: 104, position: 3, relativePosition: 3},
-          {flex_category: 'Y', id: 103, position: 4, relativePosition: 4}
+          {lesson_group: 'X', id: 101, position: 1, relativePosition: 1},
+          {lesson_group: 'X', id: 102, position: 2, relativePosition: 2},
+          {lesson_group: 'X', id: 104, position: 3, relativePosition: 3},
+          {lesson_group: 'Y', id: 103, position: 4, relativePosition: 4}
         ],
-        state.stages,
+        state.lessons,
         'second move changes group but not position'
       );
 
-      position = state.stages.find(s => s.id === id).position;
+      position = state.lessons.find(s => s.id === id).position;
       state = reducer(state, moveLesson(position, 'up'));
       assert.deepEqual(
         [
-          {flex_category: 'X', id: 101, position: 1, relativePosition: 1},
-          {flex_category: 'X', id: 104, position: 2, relativePosition: 2},
-          {flex_category: 'X', id: 102, position: 3, relativePosition: 3},
-          {flex_category: 'Y', id: 103, position: 4, relativePosition: 4}
+          {lesson_group: 'X', id: 101, position: 1, relativePosition: 1},
+          {lesson_group: 'X', id: 104, position: 2, relativePosition: 2},
+          {lesson_group: 'X', id: 102, position: 3, relativePosition: 3},
+          {lesson_group: 'Y', id: 103, position: 4, relativePosition: 4}
         ],
-        state.stages,
+        state.lessons,
         'third move changes group but not position'
       );
     });
@@ -150,12 +151,12 @@ describe('editorRedux reducer tests', () => {
         let state = reducer(initialState, setLessonGroup(2, 'Z'));
         assert.deepEqual(
           [
-            {flex_category: 'X', id: 101, position: 1, relativePosition: 1},
-            {flex_category: 'Y', id: 103, position: 2, relativePosition: 2},
-            {flex_category: 'Y', id: 104, position: 3, relativePosition: 3},
-            {flex_category: 'Z', id: 102, position: 4, relativePosition: 4}
+            {lesson_group: 'X', id: 101, position: 1, relativePosition: 1},
+            {lesson_group: 'Y', id: 103, position: 2, relativePosition: 2},
+            {lesson_group: 'Y', id: 104, position: 3, relativePosition: 3},
+            {lesson_group: 'Z', id: 102, position: 4, relativePosition: 4}
           ],
-          state.stages
+          state.lessons
         );
       });
 
@@ -163,12 +164,12 @@ describe('editorRedux reducer tests', () => {
         const newState = reducer(initialState, setLessonGroup(4, 'X'));
         assert.deepEqual(
           [
-            {flex_category: 'X', id: 101, position: 1, relativePosition: 1},
-            {flex_category: 'X', id: 102, position: 2, relativePosition: 2},
-            {flex_category: 'X', id: 104, position: 3, relativePosition: 3},
-            {flex_category: 'Y', id: 103, position: 4, relativePosition: 4}
+            {lesson_group: 'X', id: 101, position: 1, relativePosition: 1},
+            {lesson_group: 'X', id: 102, position: 2, relativePosition: 2},
+            {lesson_group: 'X', id: 104, position: 3, relativePosition: 3},
+            {lesson_group: 'Y', id: 103, position: 4, relativePosition: 4}
           ],
-          newState.stages
+          newState.lessons
         );
       });
     });
